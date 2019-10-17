f277b1a12e678d53b8271304795fe1bc5f0c3a1e
shuledelizi <201811061193@mail.bnu.edu.cn>
2019-10-17 23:30:52 +0800

    try

---
+++ ex1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 23:22:36 2019
+
+@author: Mechrevo
+"""
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
