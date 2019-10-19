d41a0d5c15d0e2fc30c34825aa1e2aa925f95e2c
shuledelizi <201811061193@mail.bnu.edu.cn>
2019-10-19 15:41:30 +0800

    miaow

---
+++ ex2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 15:01:51 2019
+
+@author: Mechrevo
+"""
+
+#A comment, this is so you can read your program later.
+#Anything after the # is ignored by python.
+print("I could have code like this.")#and the comment after
+# You can also use a comment to "disable" or comment out code
+#print("This won't run.")
+
+print("This will run")
